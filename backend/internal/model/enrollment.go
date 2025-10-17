7e1d485e356c3b3973cbdd299073ed24f5fc4185
yyq <3388675532@qq.com>
2025-10-17 16:43:55 +0800

    fix: 选课删去无用列

--- backend/internal/model/enrollment.go
+++ backend/internal/model/enrollment.go
@@ -16,6 +16,4 @@ type Enrollment struct {
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 	DeletedAt  gorm.DeletedAt `gorm:"index"` // 软删除
-	// MySQL 唯一索引
-	UniqueStudentCourse string `gorm:"uniqueIndex:idx_student_course;type:varchar(191)"` // 防止重复选课
 }
